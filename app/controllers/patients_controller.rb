acf78ad57e0c19687841f84acb0f4417b5a10089
acg2176 <acg2176@columbia.edu>
2020-07-27 16:23:24 -0400

    add slug and login html

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -26,4 +26,6 @@ class PatientsController < ApplicationController
     end
 
 
+
+
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,6 +3,14 @@ class Patient < ActiveRecord::Base
     has_many :therapists, through: :appointments
     has_secure_password
 
+    def slug
+        @slug = self.username.split().join("-")
+      end
+    
+      def self.find_by_slug(slug)
+       self.all.detect {|patient| patient.slug == slug}
+      end
+
 end
 
 
