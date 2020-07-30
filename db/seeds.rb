fcefa1dab3f053e1bc15ce71627ce97fa9d7bc0b
acg2176 <acg2176@columbia.edu>
2020-07-30 01:02:40 -0400

    add seed data

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+therapist1 = Therapist.create(:name => "Triza Guerrero", :bio => "Triza Guerrero is a licensed psychologist and a MA degree holder in clinical psychology from Columbia University.", :email => "trizag1993@yahoo.com", :location => "Makati City, Philippines")
+
+therapist2 = Therapist.create(:name => "April Richardson", :bio => "April Richardson specializes in psychotherapy and has worked with young children for 15 years.", :email => "aprilrichardson@gmail.com", :location => "Bronx, NY, USA")
+
+therapist3 = Therapist.create(:name => "Irene de Leon", :bio => "Irene de Leon received her PhD from the Univeristy of New South Wales in behavioral psychology. She has presented her dissertation at numerous conferences around the world.", :email => "idl345@gmail.com", :location => "Singapore City, Singapore")
+
+therapist4 = Therapist.create(:name => "Jordan Peterson", :bio => "Jordan Peterson is a renowned author and clinical psychologist who is currently a professor at the University of Toronto. His lectures have received over 1 million views on YouTube and his book '12 Rules for Life' has sold thousands of copies worldwide.", :email => "jbpeterson@gmail.com", :location => "Toronto, Canada")
